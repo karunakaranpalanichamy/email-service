f5731c7f3a41914dfb2171e6c4c610936a049ec1
karunakaran <karunakaranrpk@gmail.com>
2026-07-05 15:33:44 -0400

    Email service

---
+++ src/main/java/com/karuna/email_service/config/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.karuna.email_service.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // message converter
+    @Bean
+    public MessageConverter converter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    // configure RabbitTemplate
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
+
+
